c23c2658443ebd43bd3a645150793f13db41d85a
VictorMora97 <32451998+VictorMora97@users.noreply.github.com>
2019-10-05 22:28:03 +0200

    Asignacion de valores al artefacto cubo

--- CuboRubik/src/Cubo.java
+++ CuboRubik/src/Cubo.java
@@ -14,6 +14,8 @@ public class Cubo {
 
 		public static void main(String[] args) {
 			
+			int dimension;
+			int contador = 0;
             String stringCubo = "";
             String sucia;
             String ubicacionJSON = "C:\\Users\\victo\\eclipse-workspace\\Sistemas-Inteligentes\\src\\cube.json";
@@ -24,22 +26,22 @@ public class Cubo {
 
 	            JSONObject jsonObject = (JSONObject) parser.parse(reader);
 
-	            JSONArray back = (JSONArray) jsonObject.get("BACK");
+	            JSONArray back = (JSONArray) jsonObject.get("BACK"); //0
 	            System.out.println(back);
 	            
-	            JSONArray down = (JSONArray) jsonObject.get("DOWN");
+	            JSONArray down = (JSONArray) jsonObject.get("DOWN"); //1
 	            System.out.println(down);
 	            
-	            JSONArray front = (JSONArray) jsonObject.get("FRONT");
+	            JSONArray front = (JSONArray) jsonObject.get("FRONT"); //2
 	            System.out.println(front);
 	            
-	            JSONArray left = (JSONArray) jsonObject.get("LEFT");
+	            JSONArray left = (JSONArray) jsonObject.get("LEFT"); //3
 	            System.out.println(left);
 	            
-	            JSONArray right = (JSONArray) jsonObject.get("RIGHT");
+	            JSONArray right = (JSONArray) jsonObject.get("RIGHT"); //4
 	            System.out.println(right);
 
-	            JSONArray up = (JSONArray) jsonObject.get("UP");
+	            JSONArray up = (JSONArray) jsonObject.get("UP"); //5
 	            System.out.println(up);                        
 	      
 	            for(int i=0; i<back.size(); i++){    	
@@ -72,18 +74,37 @@ public class Cubo {
 	                stringCubo = stringCubo + Limpiar(sucia);
 	            }
 	            
-	            System.out.println(stringCubo);
-
+	            dimension = (stringCubo.length())/18;
+	            String array[] = stringCubo.split("");
+	            
+	/* 
+	 * Tras el proceso de conversion del JSON a un string, se asignan los valores a cada "mini-cubo" correspondiente.
+	 * El cubo se representa con un array tridimensional --> cubo[a][b][c], siendo 'a' el ID correspondiente a cada cara,
+	 * b el numero de fila dentro de esa cara, y c la columna dentro de esa fila
+	 */
+	            
+	            String[][][] cubo = new String[6][dimension][dimension];  
+	           
+    				for(int a=0; a<6; a++) { //ID de caras (BACK=0, DOWN=1...etc)
+    					for(int b=0; b<dimension; b++) { //Fila
+    						for(int c=0; c<dimension; c++) { //Columna
+    							
+		            			cubo[a][b][c]=array[contador];
+	            				contador = contador + 1;
+	            				
+	            			}
+	            		}            				
+	            	}	
+    				
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        } catch (ParseException e) {
 	            e.printStackTrace();
 	        }
 	}
-
-		public static String Limpiar(String str) { //Limpia comas y corchetes
+		
+		public static String Limpiar(String str) { 
             String limpia = ((str.replaceAll(",","") ).replaceAll("\\[", "")).replaceAll("\\]", "");
             return limpia;
 		}
-
 }
