e97b82c64f44ab7693ee198efd15b7fe04f0efa5
VictorMora97 <32451998+VictorMora97@users.noreply.github.com>
2019-10-05 20:19:32 +0200

    JSON a String
    Pasa de JSON a un unico string, que representa el estado inicial del cubo

--- CuboRubik/src/Cubo.java
+++ CuboRubik/src/Cubo.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.json.simple.JSONArray;
@@ -12,10 +13,14 @@ import org.json.simple.parser.ParseException;
 public class Cubo {
 
 		public static void main(String[] args) {
+			
+            String stringCubo = "";
+            String sucia;
+            String ubicacionJSON = "C:\\Users\\victo\\eclipse-workspace\\Sistemas-Inteligentes\\src\\cube.json";
 
 	        JSONParser parser = new JSONParser();
-
-	        try (Reader reader = new FileReader("C:\\Users\\victo\\eclipse-workspace\\Sistemas-Inteligentes\\src\\cube.json")) {
+	        
+	        try (Reader reader = new FileReader(ubicacionJSON)) {
 
 	            JSONObject jsonObject = (JSONObject) parser.parse(reader);
 
@@ -35,24 +40,51 @@ public class Cubo {
 	            System.out.println(right);
 
 	            JSONArray up = (JSONArray) jsonObject.get("UP");
-	            System.out.println(up);
-	                        
-	            
+	            System.out.println(up);                        
 	      
-	            for(int i=0; i<back.size(); i++){
-	                Object linea = back.get(i);
-	                System.out.println(linea);
-	                
-	                
+	            for(int i=0; i<back.size(); i++){    	
+	                sucia = (back.get(i)).toString();
+	                stringCubo = stringCubo + Limpiar(sucia);
+	            }   
+	            
+	            for(int i=0; i<down.size(); i++){    	
+	                sucia = (down.get(i)).toString();
+	                stringCubo = stringCubo + Limpiar(sucia);
 	            }
 	            
- 
+	            for(int i=0; i<front.size(); i++){    	
+	                sucia = (front.get(i)).toString();
+	                stringCubo = stringCubo + Limpiar(sucia);
+	            }
+	            
+	            for(int i=0; i<left.size(); i++){    	
+	                sucia = (left.get(i)).toString();
+	                stringCubo = stringCubo + Limpiar(sucia);
+	            }
+	            
+	            for(int i=0; i<right.size(); i++){    	
+	                sucia = (right.get(i)).toString();
+	                stringCubo = stringCubo + Limpiar(sucia);
+	            }
+	            
+	            for(int i=0; i<up.size(); i++){    	
+	                sucia = (up.get(i)).toString();
+	                stringCubo = stringCubo + Limpiar(sucia);
+	            }
+	            
+	            System.out.println(stringCubo);
+
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        } catch (ParseException e) {
 	            e.printStackTrace();
 	        }
-	    
+
 	}
+
+		public static String Limpiar(String str) {
+            String limpia = ((str.replaceAll(",","") ).replaceAll("\\[", "")).replaceAll("\\]", "");
+            return limpia;
+		}
 
 }
