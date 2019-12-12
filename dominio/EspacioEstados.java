9904d54e5980440dd0865bedc132c16b4e69fa47
VictorMora97 <32451998+VictorMora97@users.noreply.github.com>
2019-12-12 20:11:46 +0100

    Sucesores en orden correcto

--- dominio/EspacioEstados.java
+++ dominio/EspacioEstados.java
@@ -10,6 +10,7 @@ import persistencia.Tool;
 public class EspacioEstados {
     private static Estado estadoInicial;
     
+
     public EspacioEstados(String ruta) throws FileNotFoundException {
 
 			estadoInicial = new Estado("inicio",AgentJSON.getCube(ruta),0);
@@ -24,16 +25,16 @@ public class EspacioEstados {
     	Cube cube = e.getCube();
     	Cube aux;
 
-    	for(int i = 0;i<cube.getN();i++) {    	
+    	/*for(int i = 0;i<cube.getN();i++) {    	
    			aux = cube.clone(); aux.B(i); result.add(new Estado("B"+i,aux,1));
     		aux = cube.clone(); aux.b(i); result.add(new Estado("b"+i,aux,1));
     		aux = cube.clone(); aux.D(i); result.add(new Estado("D"+i,aux,1));
     		aux = cube.clone(); aux.d(i); result.add(new Estado("d"+i,aux,1));
     		aux = cube.clone(); aux.L(i); result.add(new Estado("L"+i,aux,1));
     		aux = cube.clone(); aux.l(i); result.add(new Estado("l"+i,aux,1));
-    	}  
+    	}  */
     	
-    	/*for(int i = 0;i<cube.getN();i++) {
+    	for(int i = 0;i<cube.getN();i++) {
     		aux = cube.clone(); aux.B(i); result.add(new Estado("B"+i,aux,1));
     	}
     	for(int i = 0;i<cube.getN();i++) {
@@ -50,8 +51,8 @@ public class EspacioEstados {
     	}
     	for(int i = 0;i<cube.getN();i++) {
     		aux = cube.clone(); aux.l(i); result.add(new Estado("l"+i,aux,1));
-    	} */
-    	  	
+    	} 
+    	
     	return result;
     }
     
